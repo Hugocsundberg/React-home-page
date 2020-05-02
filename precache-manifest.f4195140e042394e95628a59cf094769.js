2bd823a326f633500feddb691e37c47ca2020e51
Hugocsundberg <hugocsundberg@gmail.com>
2020-05-02 23:10:50 +0200

    Updates

---
+++ precache-manifest.f4195140e042394e95628a59cf094769.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106d56a1ffe39b4fc3a1ecf95e565aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fd3a4cd1b95be661ec1",
+    "url": "/static/css/main.7454fdcd.chunk.css"
+  },
+  {
+    "revision": "d0a1aca45123aff08c30",
+    "url": "/static/js/2.e89fda18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e89fda18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd3a4cd1b95be661ec1",
+    "url": "/static/js/main.a2f9087a.chunk.js"
+  },
+  {
+    "revision": "98245e3e6d8ddbe1da03",
+    "url": "/static/js/runtime-main.3522fae0.js"
+  },
+  {
+    "revision": "6a6bcaaf105ae40ce4fbff6ab7b3838c",
+    "url": "/static/media/animalvideo.6a6bcaaf.mp4"
+  },
+  {
+    "revision": "e461014eaf586beb41ca037029f07666",
+    "url": "/static/media/react.e461014e.png"
+  },
+  {
+    "revision": "2f4ac38fe39c7c80401b30ae174029cc",
+    "url": "/static/media/video.2f4ac38f.mp4"
+  }
+]);
