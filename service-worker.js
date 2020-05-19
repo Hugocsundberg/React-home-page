c9d8f487c7514949ae7ee22dae0a9773d9c2fba8
Hugocsundberg <hugocsundberg@gmail.com>
2020-05-19 18:47:38 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-home-page/precache-manifest.b37b896b1db4d4f71ede5bfdfa75f0fb.js"
+  "/React-home-page/precache-manifest.0ea724c4e034557616f0557b40458508.js"
 );
 
 self.addEventListener('message', (event) => {
